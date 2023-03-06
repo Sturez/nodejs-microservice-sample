e104ad846c8b0db99b752ce21837a4d5b332b43e
Sturez <sturez@msn.com>
2023-03-06 16:27:04 +0100

    passing mongo db curi via yaml file

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,13 @@ const start = async () => {
         throw new Error("JWT Key env variable must be defined");
     }
 
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI env variable must be defined");
+    }
+
     try {
         console.log("Connecting to mongo");
-        await mongoose.connect('mongodb://auth-mongo-srv');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected!");
     } catch (error) {
         console.error(error);
