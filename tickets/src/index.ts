8d25c993631a14809d39759e7fc1e191e89863ec
Sturez <sturez@msn.com>
2023-03-10 12:49:46 +0100

    added env varialbes to use properly NATS client

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,16 +11,30 @@ const start = async () => {
         throw new Error("MONGO_URI env variable must be defined");
     }
 
+    if (!process.env.NATS_URL) {
+        throw new Error("NATS_URL env variable must be defined");
+    }
+
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error("NATS_CLUSTER_ID env variable must be defined");
+    }
+
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error("NATS_CLUSTER_ID env variable must be defined");
+    }
+
     try {
 
         console.log("Connecting to NATS");
-        await natsWrapper.connect('ticketing','123321','http://nats-srv:4222');
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL);
 
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed');
             process.exit();
         });
-        
+
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
 
