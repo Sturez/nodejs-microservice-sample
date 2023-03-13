ad136f9e4236ddd1aafdda0334c27c034151d549
Sturez <sturez@msn.com>
2023-03-13 11:55:21 +0100

    added orders service infrastructure

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { currentUser, errorHandler, NotFound } from '@sturez-org/common';
+// import { createTicketRouter } from './routes/createTicket';
+// import { getTicketRouter } from './routes/getTicket';
+// import { indexTicketRouter } from './routes';
+// import { updateTicketRouter } from './routes/updateTicket';
+
+
+const app = express();
+// we need the app to trust Nginx
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}));
+
+app.use(currentUser);
+
+// app.use(createTicketRouter);
+// app.use(getTicketRouter);
+// app.use(updateTicketRouter);
+// app.use(indexTicketRouter);
+
+
+app.all('*', async (req, res) => {
+    throw new NotFound();
+});
+
+app.use(errorHandler);
+
+export { app };
