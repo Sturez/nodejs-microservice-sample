c5f1106937ad106fea71bd1b7238543133f7dd01
Sturez <sturez@msn.com>
2023-03-18 13:36:42 +0100

    updated base listener and base publisher to expose nats client to child classes.

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@sturez-org/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 import { queueGroupName } from "./queueGroupNames";
 
 
@@ -19,6 +20,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
         // mark the ticket as reserved
         ticket.orderId = data.id;
         await ticket.save();
+
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            version: ticket.version,
+            orderId: ticket.orderId
+        });
 
         //ack the messaging server
         msg.ack();
