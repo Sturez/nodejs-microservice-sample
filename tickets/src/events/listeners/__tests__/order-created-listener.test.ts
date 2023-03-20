0d215668913976b9b5ec0d5a5ff36128e0a77e60
Sturez <sturez@msn.com>
2023-03-20 11:12:58 +0100

    test that creating an order emits an event ticket updated

--- tickets/src/events/listeners/__tests__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__tests__/order-created-listener.test.ts
@@ -58,10 +58,17 @@ it('acks the message', async () => {
     expect(msg.ack).toHaveBeenCalled();
 });
 
-// it('', async () => {
+it('publishes a ticket updated event', async () => {
+    const { listener, ticket, data, msg } = await setup();
 
-//  });
+    // handle an order created event
+    await listener.onMessage(data, msg);
 
-// it('', async () => {
+    // checks that a Ticket Updated event is being published
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+    
+    //retrieve Ticket updated event and check that OrderId is the same.
+    const ticketUpdatedData = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
 
-//  });
+    expect(data.id).toEqual(ticketUpdatedData.orderId);
+});
